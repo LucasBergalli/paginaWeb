7332a49713c15d689cf42176f9a871aac554ee8e
Lucas Bergalli <lucas.bergalli@ibm.com>
2024-10-18 18:54:24 -0300

    varios

--- script.js
+++ script.js
@@ -3,16 +3,13 @@ const images = document.querySelectorAll('.carousel-images img');
 const totalImages = images.length;
 
 document.querySelector('.next').addEventListener('click', () => {
+    images[currentIndex].style.display = 'none';
     currentIndex = (currentIndex + 1) % totalImages;
-    updateCarousel();
+    images[currentIndex].style.display = 'block';
 });
 
 document.querySelector('.prev').addEventListener('click', () => {
+    images[currentIndex].style.display = 'none';
     currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-    updateCarousel();
+    images[currentIndex].style.display = 'block';
 });
-
-function updateCarousel() {
-    const offset = -currentIndex * 100;
-    document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
-}
